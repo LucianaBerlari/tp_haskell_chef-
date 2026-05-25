eb2f87e047e7bbc384e645a722b348560ed67a4b
Luciana Berlari <lberlari@frba.utn.edu.ar>
2026-05-25 20:01:35 -0300

    Borré las respuestas de las preguntas finales

--- src/Library.hs
+++ src/Library.hs
@@ -147,18 +147,3 @@ platinum = Plato {
     dificultad=10, 
     componentes = infinitosComponentesMisteriosos}
 
-{-Preguntas:
-
-¿Qué sucede si aplicamos cada uno de los trucos de la Parte A al Platinum?
-Se podrian aplicar todas porque ninguna requiere saber la totalidad de la lista. 
-En "endulzar", "salar" , "darSabor" y "duplicarPorcion" no nos interesa saber la longitud de la lista, solo se aplica cunado se requiera.
-Y por ultimo, "simplificar" se puede aplicar porque sabiendo que hay un componente 6 ya sabemos que hay mas de 5 componente y no necesitamos saber el total de elementos de la lista
-
-¿Cuáles de las preguntas de la parte B se pueden hacer sobre Platinum?
-Se pueden hacer "esComplejo" y "noAptoHipertensión" porque no requiere recorrer toda la lista como "esVegano" y "esSinTacc"
-
-¿Se puede saber si el Platinum es mejor que otro plato?
-No, porque no se puede saber la suma total de los infinitos componentes
-
-
--}
