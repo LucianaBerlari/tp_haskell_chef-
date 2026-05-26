62ffd97759f080416cb6ab37a64548bbd60c9343
Luciana Berlari <lberlari@frba.utn.edu.ar>
2026-05-25 21:58:41 -0300

    Update Spec.hs

--- src/Spec.hs
+++ src/Spec.hs
@@ -2,11 +2,10 @@
 module Spec where
 import PdePreludat
 import Library
-{-import Test.Hspec
+import Test.Hspec
 
-correrTests :: IO ()
-correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
--}
+--correrTests :: IO ()
+--correrTests = hspec $ do
+  --describe "Test de ejemplo" $ do
+    --it "El pdepreludat se instaló correctamente" $ do
+      --doble 1 `shouldBe` 2
